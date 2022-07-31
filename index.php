25e5c032c504fd077c6e369653db823fd36d60ec
MohHefni05 <mohhefni2000@gmail.com>
2022-07-30 22:24:34 -0700

    menambahkah list

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <h1>Belajar git</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, quidem optio commodi facere id provident. Repellendus esse quaerat fugiat, consequuntur libero in suscipit sed voluptatem id impedit sequi, iste voluptate minus doloremque corrupti ipsa expedita numquam est adipisci aut? At, cum maxime cumque maiores quod labore doloremque? Accusantium, voluptatum adipisci!</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat blanditiis voluptatum ad consequuntur velit earum molestias quasi hic consectetur qui.</p>
+    <li>satu</li>
+    <li>dua</li>
     
 </body>
 </html>
